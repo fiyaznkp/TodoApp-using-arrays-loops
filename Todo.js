ee4f121b99a8108364fdcbb12d2abef7f141d9e5
fiyaznkp <fiyaznkp@gmail.com>
2024-02-07 17:53:05 +0530

    updated my todo-list project, added foreach instead of forloop

--- Todo.js
+++ Todo.js
@@ -1,44 +1,3 @@
-
-
-// showTodo()
-// function showTodo(){
-  
-//   let htmlvalue =''
-//   for (let i=0;i<todoList.length;i++){
-//     let todoObject = todoList[i]
-
-
-//     let{name,dueDate}=todoObject
-
-//     let html = `<div> ${name}</div>
-//      <div>${dueDate}</div>
-//      <button onclick="todoList.splice(${i}, 1)
-//     showTodo()"
-//     class='delete-btn
-//     >Delete</button> `
-//     htmlvalue += html 
-//   }
-//   document.querySelector('.div').innerHTML = htmlvalue
-// }
-
-// function addTodo(){
-//     const inputValue = document.querySelector('.input-box')
-
-//     const dateValue = document.querySelector('.due-Date')
-
-//     const date= dateValue.value
-
-//   let todoName=  inputValue.value
-
-//   todoList.push({name:todoName ,dueDate:date})
-
-//   console.log(todoList)
-
-//   inputValue.value =''
-
-//   showTodo()
-// }
-
 let todoList =JSON.parse(localStorage.getItem('todoList')) || [{
   name:'make dinner',
   dueDate:'2024-01-24'
@@ -51,18 +10,32 @@ showTodo()
 function showTodo() {
   let htmlvalue = '';
 
-  for (let i = 0; i < todoList.length; i++) {
-    let todoObject = todoList[i];
+  todoList.forEach(function(todoObject,index){
+   
     let { name, dueDate } = todoObject;
 
     let html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button onclick="(function(index) { todoList.splice(index, 1); showTodo(); })(${i})" class='delete-btn'>Delete</button>
+      <button onclick="(function(index) { todoList.splice(index, 1); showTodo(); })(${index})" class='delete-btn'>Delete</button>
     `;
 
     htmlvalue += html;
-  }
+
+  })
+
+  // for (let i = 0; i < todoList.length; i++) {
+  //   let todoObject = todoList[i];
+  //   let { name, dueDate } = todoObject;
+
+  //   let html = `
+  //     <div>${name}</div>
+  //     <div>${dueDate}</div>
+  //     <button onclick="(function(index) { todoList.splice(index, 1); showTodo(); })(${i})" class='delete-btn'>Delete</button>
+  //   `;
+
+  //   htmlvalue += html;
+  // }
 
   document.querySelector('.div').innerHTML = htmlvalue;
 }
